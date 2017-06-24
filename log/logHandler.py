14da9a263d24b9c71dc97d681e47f3fb22ab89c2
SmallHedgehog <hedgehogcaifei@outlook.com>
2017-06-24 22:42:17 +0800

    日志记录模块

---
+++ log/logHandler.py
@@ -0,0 +1,61 @@
+# coding:utf-8
+
+"""
+实现日记记录模块
+"""
+
+__author__ = 'hedgehog'
+
+import logging
+import os
+from logging.handlers import TimedRotatingFileHandler
+
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+ROOT_PATH = os.path.join(CURRENT_PATH, os.pardir)
+LOG_PATH = os.path.join(ROOT_PATH, 'log')
+
+class logHandler(logging.Logger):
+    def __init__(self, name, level = logging.DEBUG):
+        self.name = name
+        self.level = level
+        logging.Logger.__init__(self, name = name, level = level)
+        self.__setFileHandler__()
+        self.__setStreamHandler__()
+
+    def __setFileHandler__(self, level=None):
+        """
+        set file handler
+        :param level:
+        :return:
+        """
+        file_name = os.path.join(LOG_PATH, '{name}.log'.format(name=self.name))
+        # 设置日志回滚, 保存在log目录, 一天保存一个文件, 保留15天
+        file_handler = TimedRotatingFileHandler(filename=file_name, when='D', interval=1, backupCount=15)
+        file_handler.suffix = '%Y%m%d.log'
+        if not level:
+            file_handler.setLevel(self.level)
+        else:
+            file_handler.setLevel(level)
+        formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+
+        file_handler.setFormatter(formatter)
+        self.addHandler(file_handler)
+
+    def __setStreamHandler__(self, level=None):
+        """
+        set stream handler
+        :param level:
+        :return:
+        """
+        stream_handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+        stream_handler.setFormatter(formatter)
+        if not level:
+            stream_handler.setLevel(self.level)
+        else:
+            stream_handler.setLevel(level)
+        self.addHandler(stream_handler)
+
+if __name__ == '__main__':
+    log = logHandler(__name__)
+    log.info('this is a test msg')
